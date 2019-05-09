0483ee21fe09c6fe6bbc8a7360da46cd7683928e
heiseish <dtrnggiang@gmail.com>
2019-05-09 16:15:56 +0700

    remove codeforce test

--- test/externalApis/codeforce.test.ts
+++ test/externalApis/codeforce.test.ts
@@ -11,43 +11,43 @@ const useInTest = (): void => {
 	})
 }
 
-describe('Codeforce Api ⌨️', function () {
-	this.timeout(CODEFORCE_API_RESPONSE_TIME)
-	useInTest()
+// describe('Codeforce Api ⌨️', function () {
+// 	this.timeout(CODEFORCE_API_RESPONSE_TIME)
+// 	useInTest()
 
-	describe('getUserRating()',  function () {
-		it('Should get a ranking object',  function() {
-			expect(this.user1)
-			.to.be.an('object')
+// 	describe('getUserRating()',  function () {
+// 		it('Should get a ranking object',  function() {
+// 			expect(this.user1)
+// 			.to.be.an('object')
 
-			expect(this.user2)
-			.to.be.an('object')
-		})
+// 			expect(this.user2)
+// 			.to.be.an('object')
+// 		})
 		
 
-		it('Should return a different ranking object for each user',  function() {
-			expect(this.user1)
-			.to.not.be.deep.equal(this.user2)
-		})
+// 		it('Should return a different ranking object for each user',  function() {
+// 			expect(this.user1)
+// 			.to.not.be.deep.equal(this.user2)
+// 		})
 
-		it('Returned object should have rating property', function() {
-			expect(this.user1)
-			.to.have.property('rating')
-			.that.is.a('number')
+// 		it('Returned object should have rating property', function() {
+// 			expect(this.user1)
+// 			.to.have.property('rating')
+// 			.that.is.a('number')
 
-			expect(this.user2)
-			.to.have.property('rating')
-			.that.is.a('number')
-		})
+// 			expect(this.user2)
+// 			.to.have.property('rating')
+// 			.that.is.a('number')
+// 		})
 		
-		it('Returned object should have rank property', function() {
-			expect(this.user1)
-			.to.have.property('rank')
-			.that.is.a('string')
+// 		it('Returned object should have rank property', function() {
+// 			expect(this.user1)
+// 			.to.have.property('rank')
+// 			.that.is.a('string')
 
-			expect(this.user2)
-			.to.have.property('rank')
-			.that.is.a('string')
-		})
-	})
-})
+// 			expect(this.user2)
+// 			.to.have.property('rank')
+// 			.that.is.a('string')
+// 		})
+// 	})
+// })
